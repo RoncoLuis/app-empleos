9f139bf4ca340aa0b9d666ae24d7d518689fa2bf
RoncoLuis <luiseduardoronquillo@hotmail.com>
2022-05-23 17:37:22 -0500

    Volver aqui para revisar Practica para renderizar informacion en template html como argumento y dentro de las etiquetas

--- src/main/java/com/example/appempleos/controller/HomeController.java
+++ src/main/java/com/example/appempleos/controller/HomeController.java
@@ -4,14 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
 //@RestController with RestController we cannot return a view
 @Controller //we can return an html view
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("mensaje","Hola Thymeleaf");
-        model.addAttribute("fecha", new java.util.Date());
+        //model.addAttribute("mensaje","Hola Thymeleaf");
+       // model.addAttribute("fecha", new java.util.Date());
+        String nombre = "Luis";
+        String puesto = "Programador Jr.";
+        Date fechaPub = new Date();
+        double salario = 4500.00;
+        boolean vigencia = true;
+
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("puesto", puesto);
+        model.addAttribute("fechaPublicacion", fechaPub);
+        model.addAttribute("salario", salario);
+        model.addAttribute("vigencia", vigencia);
+
         return "home";
     }
 }
