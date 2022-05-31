8b0906f4e3098e40ac5d943dc0761c5013da049e
RoncoLuis <luiseduardoronquillo@hotmail.com>
2022-05-30 19:30:15 -0500

    Practica usando @RequestParameter y metodo get

---
+++ src/main/java/com/example/appempleos/controller/VacantesController.java
@@ -0,0 +1,25 @@
+package com.example.appempleos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/vacantes")
+public class VacantesController {
+
+    @GetMapping(path = "/view/{id}")
+    public String verDetalle(@PathVariable(name = "id") int id, Model model){
+        System.out.println("El id de la vacante es: "+id);
+        model.addAttribute("idVacante",id);
+        return "vacantes/detalle-vacante";
+    }
+
+    @GetMapping(path = "/eliminar-vacante")
+    public String eliminarVacante(@RequestParam(name = "id") int idVacante, Model model){
+        System.out.println("Se eliminara la vacante con el id: "+ idVacante);
+        model.addAttribute("idVacante",idVacante);
+        return "vacantes/mensaje-eliminar";
+    }
+
+}
